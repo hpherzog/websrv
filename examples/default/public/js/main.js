f3f76754b33e251db3ea1c39778c04ac97701216
Hans-Peter Herzog <hans-peter.herzog@cdn-hph.de>
2014-08-14 22:03:21 +0200

    Keepalive

--- examples/default/public/js/main.js
+++ examples/default/public/js/main.js
@@ -3,4 +3,23 @@ var client = new websrv.Client({
     url: 'wss://test1.tld:8433'
 });
 
+client.on('online', function() {
+    console.log('Client online');
+});
+
+client.on('connecting', function() {
+    console.log('Client connecting');
+});
+
+client.on('alive', function() {
+    console.log('Client alive');
+});
+
+client.on('offline', function() {
+    console.log('Client offline');
+});
+
+client.on('error', function() {
+});
+
 client.open();
