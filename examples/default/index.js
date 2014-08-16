c9ac01aa0bbc40fbac553ca7188a8a00dda5296f
Hans-Peter Herzog <hans-peter.herzog@cdn-hph.de>
2014-08-16 11:57:52 +0200

    Added ssl test certs

--- examples/default/index.js
+++ examples/default/index.js
@@ -6,9 +6,11 @@ var cwd = process.cwd();
 var srv = new websrv.Server({
     port: 8080,
     sslPort: 8433,
-    sslKeyPath: cwd + '/ssl/test.key',
-    sslCertPath: cwd + '/ssl/test.crt',
-    cookieSecret: '14fff00895822c08013d750a41c8f024'
+    sslKeyPath: cwd + '/ssl/websrv.key',
+    sslCertPath: cwd + '/ssl/websrv.crt',
+    cookieSecret: '14fff00895822c08013d750a41c8f024',
+    viewEngine: 'ejs',
+    viewPath: './views'
 });
 
 srv.app.get('/', function(req, res) {
